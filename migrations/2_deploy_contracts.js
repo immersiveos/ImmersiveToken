3b0879e6e9ed518ac64a6bb0586a5d56957c2b7e
imosdevteam <devteam@immersiveos.com>
2017-06-08 15:17:18 -0400

    Fix deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,45 +1,53 @@
-let ImmersiveToken = artifacts.require("./ImmersiveToken.sol");
-let log = console.log;
+const ImmersiveToken = artifacts.require("./ImmersiveToken.sol");
+const log = console.log;
 
-let Web3 = require('../node_modules/web3');
-let web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+const Web3 = require('../node_modules/web3');
+const web3 = new Web3();
+const BigNumber = web3.BigNumber;
 
 module.exports = function(deployer, network, accounts) {
 
   if (network === 'development') {
     log("Dev deployment...");
 
+    web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+
     const opsAddress = accounts[0];
     const endBlock = web3.eth.blockNumber + 100;
-    const fundingGoal = 100;
+    const fundingGoal = web3.toWei(new BigNumber(1), "ether");
+    const startblock = web3.eth.blockNumber;
 
-    log ("End block:" + endBlock);
-    log ("Ops account: " + accounts[0])
-    log ("Funding goal:" + fundingGoal)
+    log (`Start block: ${startblock}`);
+    log (`End block: ${endBlock}`);
+    log (`Ops account: ${opsAddress}`);
+    log (`Funding goal: ${fundingGoal}`);
 
-    deployer.deploy(ImmersiveToken,accounts[0],fundingGoal,endBlock);
+    deployer.deploy(ImmersiveToken, accounts[0], fundingGoal, endBlock);
 
   } else if (network === 'live') {
 
     log("Livenet deployment...");
 
+    web3.setProvider(new web3.providers.HttpProvider('http://localhost:8546'));
+
     // todo: put ops account multi-sig wallet address here
     const opsAddress = '0x95db9c1e4ca634c90333f8eded6e354ad6eba9dc';
 
-    const blocksPerMinutes = 3; // 20 secs average per block
+    const blocksPerMinutes = 3.5; // assume 20 secs average per block
     const blocksPerHour = blocksPerMinutes * 60;
     const blocksPerDay = blocksPerHour * 24;
-    const blocksInMonth = blocksPerDay * 30;
+    const blocksInMonth = blocksPerDay * 31; // july and august are 31 days each
     const campaginDurationMonths = 2;
 
+    const startblock = web3.eth.blockNumber;
     const endBlock = web3.eth.blockNumber.add(blocksInMonth * campaginDurationMonths);
 
     const fundingGoal = web3.toWei(new BigNumber(20000), "ether");
 
-    log ("End block:" + endBlock);
-    log ("Ops account: " + opsAddress);
-    log ("Funding goal:" + fundingGoal);
+    log (`Start block: ${startblock}`);
+    log (`End block: ${endBlock}`);
+    log (`Ops account: ${opsAddress}`);
+    log (`Funding goal: ${fundingGoal}`);
 
     /// todo: put deployer account here
     const deployerAccount = '0x95db9c1e4ca634c90333f8eded6e354ad6eba9dc';
